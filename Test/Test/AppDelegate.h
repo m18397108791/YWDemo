d2201b0a6ec59f295ea636f1f36ba49bcb2117bb
XW <XW@xwdemac-mini-2.local>
2016-10-01 13:39:58 +0800

    建立工程

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by XW on 16/10/1.
+//  Copyright © 2016年 yw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by XW on 16/10/1.
+//  Copyright © 2016年 yw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
